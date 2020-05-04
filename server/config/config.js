6992dd90048fd3b0bb92f45eb378a0d7da232062
Anderson <anderson.sanchez.toledo@gmail.com>
2020-05-04 20:51:28 +0200

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,21 @@
 // Puerto
 // ===========================
 process.env.PORT = process.env.PORT || 3000
+
+// ===========================
+// Entrno
+// ===========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+// ===========================
+// Base de datos
+// ===========================
+let urlDB
+
+if ('dev' === process.env.NODE_ENV) {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://andersato:qaLvnEtnmxkrhEnZ@cluster0-pkltk.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+require('./config/config')
+
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 const app = express()
-require('./config/config')
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -9,36 +11,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', (req, res) => {
-    res.json('get usuario')
-})
+app.use(require('./routes/usuario'))
 
-app.post('/usuario', (req, res) => {
-
-    let body = req.body
-
-    if (undefined === body.nombre) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-})
-
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id
-
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuario', (req, res) => {
-    res.json('delete usuario')
+mongoose.set('useCreateIndex', true)
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(res => {
+    console.log('Base de datos ONLINE');
+}).catch(err => {
+    throw err
 })
 
 app.listen(process.env.PORT, () => {
