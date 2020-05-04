125c9ea9c26b44fd7161b758b26027c2daf53473
Anderson <anderson.sanchez.toledo@gmail.com>
2020-05-04 21:12:51 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB
 if ('dev' === process.env.NODE_ENV) {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://andersato:qaLvnEtnmxkrhEnZ@cluster0-pkltk.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB
